899acd7bee13db0b6c120ca34f1021246c4d3ae6
Chuck Yount <chuck.yount@intel.com>
2018-08-14 08:58:11 -0700

    Update API docs

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['idx_5ft',['idx_t',['../group__yask.html#ga3820f8c6b5f6a92c0df31746b7d2891b',1,'yask']]],
-  ['is_5fauto_5ftuner_5fenabled',['is_auto_tuner_enabled',['../classyask_1_1yk__solution.html#a09f2f773e4e8d63c6d1f3b6486bd08bf',1,'yask::yk_solution']]],
+  ['is_5fauto_5ftuner_5fenabled',['is_auto_tuner_enabled',['../classyask_1_1yk__solution.html#a150bb5f7d351afd9b64744f32fea4f5c',1,'yask::yk_solution']]],
   ['is_5fdependency_5fchecker_5fenabled',['is_dependency_checker_enabled',['../classyask_1_1yc__solution.html#a894a0f5b85744190a56c1a64b5eaaff9',1,'yask::yc_solution']]],
   ['is_5fdim_5fused',['is_dim_used',['../classyask_1_1yk__grid.html#aeed4acdba5c91a92309132b19cb00c2a',1,'yask::yk_grid']]],
   ['is_5felement_5fallocated',['is_element_allocated',['../classyask_1_1yk__grid.html#a575ac2bbd6ba7b7e07a71d3266a93c8d',1,'yask::yk_grid::is_element_allocated(const std::vector&lt; idx_t &gt; &amp;indices) const  =0'],['../classyask_1_1yk__grid.html#a24bf9ab4fbea6bfcc35264a4745cddc0',1,'yask::yk_grid::is_element_allocated(const std::initializer_list&lt; idx_t &gt; &amp;indices) const  =0']]],
